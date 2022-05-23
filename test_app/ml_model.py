10bbc2b9a247a5bc6553f0e8ca15d15bfd0e9e52
Johnny Shollaj <xhoni.shollaj@u.nus.edu>
2022-05-23 12:49:54 +0800

    Adding the different Django files for the repo testing

---
+++ test_app/ml_model.py
@@ -0,0 +1,6 @@
+from unittest import result
+
+
+def multiplier(num):
+    result = int(num) * 111
+    return result
---
+++ test_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from . import ml_model
+
+def home(request):
+    return render(request, 'index.html')
+
+def result(request):
+    user_input = request.GET['user_input']
+    user_input = ml_model.multiplier(user_input)
+    return render(request, 'result.html', {'home_input':user_input})
